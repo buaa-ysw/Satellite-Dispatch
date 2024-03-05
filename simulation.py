788c6ed4833d11416ffe217f9ad1298b65bb271f
Your Name <you@example.com>
2024-03-05 15:23:46 +0800

    input disaster √

--- simulation.py
+++ simulation.py
@@ -2,7 +2,10 @@ from crewai import Agent, Task, Crew, Process
 import os
 
 from langchain.agents import Tool
-from langchain.utilities import GoogleSerperAPIWrapper
+from langchain_community.utilities import GoogleSerperAPIWrapper
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Set API key
 os.environ["SERPER_API_KEY"] = "f2262d553f5691749a5420e2a5d3a2b36c84aa62"
@@ -37,9 +40,9 @@ class SimulationCrew():
                         the format like this:
                         # Disaster: XXX
                         [09:00] [40.15°N, 116°E] XXX hits the city.
-                        [09:15] [40.55°N, 116°E] Buildings collapse, people are trapped.
+                        [09:15] [41.55°N, 117°E] Buildings collapse, people are trapped.
                         [09:30] [40.85°N, 116°E] Weather changes, weakening communication signals.
-                        [09:45] [41.15°N, 116°E] Road destruction, people are in panic.
+                        [09:45] [39.15°N, 116°E] Road destruction, people are in panic.
                         this is just a format, the time and location are just an example.""",
             verbose=True,
             llm=openai_gpt35, # Ollama model passed here
@@ -48,7 +51,7 @@ class SimulationCrew():
 
         self.researcher = Agent(
             role='Disaster Information Analyst',
-            goal='Find and analyze news about natural disasters and the occurrence of natural disasters',
+            goal='Find and analyze news about this kind of natural disasters and the occurrence of natural disasters',
             backstory="""You work for an international natural disaster data center. 
                         Your expertise lies in sorting out and documenting what happens at every moment and place during natural disasters.
                         You have a rich accumulation of events, understand what happens in various natural disasters, and have a knack for summarizing events.""",
@@ -102,26 +105,24 @@ class SimulationCrew():
         )
 
         self.task2 = Task(
-            description="""Find a kind of natural disasters and analyze documentarys about this.
-                        other natural disasters are also acceptable.
-                        Search for a specific disaster event and the occurrence of it,
-                        give time, location, and the event that happened.
-                        """,
-            expected_output="""Find a kind of natural disasters and analyze documentarys about this. 
-                        other natural disasters are also acceptable.
-                        Search for a specific disaster event and the occurrence of it, (as many as possible, at least 20 events) give time, location, and the event that happened.
-                        including but not limited to people being trapped, weather changes, weakening communication signals, road destruction, and more. 
-                        The format is like this:
-                        # Disaster: XXX
-                        [09:00] [40.15°N, 116°E] XXX hits the city.
-                        [09:15] [40.55°N, 116°E] Buildings collapse, people are trapped....""",
+            description="""{}, Find this kind of natural disasters and analyze documentarys about this.
+                Search for a specific disaster event and the occurrence of it,
+                give time, location, and the event that happened.
+                """.format(self.disaster),
+            expected_output="""{}, Find this kind of natural disasters and analyze documentarys about this.
+                Search for a specific disaster event and the occurrence of it, (as many as possible, at least 20 events) give time, location, and the event that happened.
+                including but not limited to people being trapped, weather changes, weakening communication signals, road destruction, and more. 
+                The format is like this:
+                # Disaster: xxx
+                [xx:xx] [xx°N, xx°E] XXX hits the city.
+                [xx:xx] [xx°N, xx°E] Buildings collapse, people are trapped....""".format(self.disaster),
             agent=self.researcher
         )
         
         self.task3 = Task(
             description="""Write more reasonable events about the disaster.
                         You will use the information provided by the Disaster Generator and the Disaster Information Analyst,
-                        including but not limited to people being trapped, weather changes, weakening communication signals, road destruction, and more.
+                        must including but not limited to people being trapped, weather changes, weakening communication signals, road destruction, and more.
                         or just create some new resonable information to create a detailed and engaging narrative.
                         The format is like this:
                         # Disaster: XXX
@@ -130,7 +131,7 @@ class SimulationCrew():
                         [time3] [°N, °E] Weather changes, weakening communication signals.
                         """,
             expected_output="""Output at least 20 events, 
-                        must be reasonable, and include the information about people being trapped, weather changes, weakening communication signals, road destruction, and more.
+                        must be reasonable, and must include the information about people being trapped, weather changes, weakening communication signals, road destruction, and more.
                         the format is like this:
                         # Disaster: XXX
                         [time1] [°N, °E] XXX hits the city.
@@ -152,5 +153,9 @@ if __name__ == "__main__":
     print("######################")
     print("## Starting Simulation")
     print("######################")
-    simulation = SimulationCrew(disaster="earthquake")
+
+    # input disaster type
+    disaster_type = input("Enter the type of disaster: ")
+
+    simulation = SimulationCrew(disaster=disaster_type)
     simulation.run()
